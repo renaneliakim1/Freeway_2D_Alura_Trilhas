4d8027595ab4cd2808489cb7f7ea761db1be635b
renaneliakim1 <renaneliakim1@gmail.com>
2024-04-10 12:56:25 -0300

    create function checkCollision

--- actors.js
+++ actors.js
@@ -1,40 +1,39 @@
 let yActor = 366;
 let xActor = 100;
 
-
-
-
 let collide = false;
 
-function showActor(){
-    image(actorImage, xActor, yActor, 30, 30);
+function showActor() {
+  image(actorImage, xActor, yActor, 30, 30);
 }
 
+function moveActor() {
+  if (keyIsDown(UP_ARROW)) {
+    yActor -= 3;
+  }
 
-
-function moveActor(){
-    if (keyIsDown(UP_ARROW)){
-        yActor -= 3;
-    }
-
-    if (keyIsDown(DOWN_ARROW)){
-        yActor += 3;
-    }
+  if (keyIsDown(DOWN_ARROW)) {
+    yActor += 3;
+  }
 }
 
-
-
-function checkCollision(){
-    for(let i = 0; i < imageCars.length; i = i + 1){    
-        collide = collideRectCircle(xCars[i], yCars[i], carLength, carHeight,xActor,yActor, 15)
-        if(collide){
-            print("collision")
-
-        }        
-        
-
-
+function checkCollision() {
+  for (let i = 0; i < imageCars.length; i = i + 1) {
+    collide = collideRectCircle(
+      xCars[i],
+      yCars[i],
+      carLength,
+      carHeight,
+      xActor,
+      yActor,
+      15
+    );
+    if (collide) {
+        collisionCar();
     }
+  }
 }
 
-
+function collisionCar(){
+  yActor = 366;
+}
--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function draw(){
     moveActor();
     returnStartingPositionCar();
     checkCollision();
-
 }
 
 
